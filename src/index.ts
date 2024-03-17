38dc5952b9425ce136518799ec6c051475c2ef69
Muhammad Awais <awaisshahid965@gmail.com>
2024-03-17 06:32:39 +0500

    feat: added mongodb connection logic

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,23 @@ require('dotenv').config()
 import 'reflect-metadata'
 import http from 'http'
 import { setupExpressServer } from './adapters/express'
+import { setupMongoDB } from './adapters/mongodb'
 
 const PORT = process.env.PORT ?? 4000
 const expressApp = setupExpressServer()
 
 const httpServer = http.createServer(expressApp)
 
-httpServer.listen(PORT, () => {
-    console.log(`Server listening at Port: ${PORT}`)
-})
+const initServer = async () => {
+    try {
+        await setupMongoDB()
+
+        httpServer.listen(PORT, () => {
+            console.log(`Server listening at Port: ${PORT}`)
+        })
+    } catch (_) {
+        process.exit(1)
+    }
+}
+
+initServer()
