3c3af51305591c6194983e11fdb928ce55633ce1
Muhammad Awais <awaisshahid965@gmail.com>
2024-03-17 07:42:09 +0500

    feat: added user model with seed script for admin user

---
+++ src/models/user.model.ts
@@ -0,0 +1,23 @@
+import { Document, Schema, model } from 'mongoose'
+import { User, UserRole } from '../types/user.types'
+
+interface UserDocument extends User, Document {}
+
+const userSchema = new Schema<UserDocument>({
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: Object.values(UserRole),
+    },
+})
+
+const UserModel = model('User', userSchema)
+export default UserModel
